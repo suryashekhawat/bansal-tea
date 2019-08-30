677481379751d41b890d4657aa3b06a091112bcb
Surya Pratap <surya@skillenza.com>
2019-08-30 21:30:52 +0530

    view template fragments add

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,6 @@
-var navbar_state = false;
-$(".nav-menu").on('click', function(e){
-  console.log(e);
-  if (navbar_state == false){
-    $("#nav-menu-items").css("display", "flex");
-    $(".navbar").css("background-color", "white");
-    $("#nav-menu-image-btn").attr("src", "images/icons/cross-black.png");
-    navbar_state = true;
-  }else{
-    $("#nav-menu-items").css("display", "none");
-    $(".navbar").css("background-color", "transparent");
-    $("#nav-menu-image-btn").attr("src", "images/icons/menu-white.png");
-    navbar_state = false;
-  }
 
-})
+
+$(document).ready(function(e){
+  $("#navbar-content").load("navbar.html");
+  $("#footer-content").load("footer.html");
+});
