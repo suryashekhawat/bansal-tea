abfa8dcbfb2d9fe32a08f6ef53d0e07e150218e2
Surya Pratap <surya@skillenza.com>
2019-08-30 11:25:58 +0530

    landing page update

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,16 @@
+var navbar_state = false;
 $(".nav-menu").on('click', function(e){
   console.log(e);
-  var navbar = $(".navbar");
-  navbar.css("background-color", "transparent");
-  $("#nav-menu-items").css("display", "none");
-  $("#nav-menu-image-btn").attr("src", "images/icons/menu-white.png");
-  
+  if (navbar_state == false){
+    $("#nav-menu-items").css("display", "flex");
+    $(".navbar").css("background-color", "white");
+    $("#nav-menu-image-btn").attr("src", "images/icons/cross-black.png");
+    navbar_state = true;
+  }else{
+    $("#nav-menu-items").css("display", "none");
+    $(".navbar").css("background-color", "transparent");
+    $("#nav-menu-image-btn").attr("src", "images/icons/menu-white.png");
+    navbar_state = false;
+  }
+
 })
