03e94b5aca67e98c360878d884433ab3a35f8d3b
Surya Pratap <surya@skillenza.com>
2019-09-04 10:40:10 +0530

    navbar

--- scripts/main.js
+++ scripts/main.js
@@ -8,10 +8,13 @@ $(window).scroll(function() {
   console.log(pos);
   if (pos>348){
     $(".navbar").css("background-color", "white");
-    $("#nav-menu-image-btn").attr("src", "images/icons/menu-black.png");
+    $("#nav-menu-image-btn").attr("src", "images/icons/cross-black.png");
+    $("#nav-menu-items").css("display", "flex");
+    $(".navbar").removeClass( "navbar-white-color" );
   }
   if (pos<348){
     $(".navbar").css("background-color", "transparent");
     $("#nav-menu-image-btn").attr("src", "images/icons/menu-white.png");
+    $(".navbar").addClass( "navbar-white-color" );
   }
 });
