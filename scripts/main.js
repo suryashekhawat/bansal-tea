8f606b1d68db75555b5b8d72806ddec46822f191
Surya Pratap <surya@skillenza.com>
2019-09-04 10:26:49 +0530

    navbar

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,16 @@ $(document).ready(function(e){
   $("#navbar-content").load("navbar.html");
   $("#footer-content").load("footer.html");
 });
+
+$(window).scroll(function() {
+  var pos = $(this).scrollTop();
+  console.log(pos);
+  if (pos>348){
+    $(".navbar").css("background-color", "white");
+    $("#nav-menu-image-btn").attr("src", "images/icons/menu-black.png");
+  }
+  if (pos<348){
+    $(".navbar").css("background-color", "transparent");
+    $("#nav-menu-image-btn").attr("src", "images/icons/menu-white.png");
+  }
+});
