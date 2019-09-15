eba2bb823d9c0ecdcd3fb055e79304c46f412beb
Surya Pratap <surya@skillenza.com>
2019-09-15 08:57:21 +0530

    menu mobile view

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,29 @@ $(document).ready(function(e){
   $("#navbar-content").load("navbar.html");
   $("#footer-content").load("footer.html");
 });
+var deviceWidth;
+$(document).ready(function(e){
+  deviceWidth = $(window).width();
+  if (deviceWidth <= 500){
 
-$(window).scroll(function() {
-  var pos = $(this).scrollTop();
-  console.log(pos);
-  if (pos>348){
-    $(".navbar").css("background-color", "white");
-    $("#nav-menu-image-btn").attr("src", "images/icons/cross-black.png");
-    $("#nav-menu-items").css("display", "flex");
-    $(".navbar").removeClass( "navbar-white-color" );
+    $(".navbar").css("display", "none");
   }
-  if (pos<348){
-    $(".navbar").css("background-color", "transparent");
-    $("#nav-menu-image-btn").attr("src", "images/icons/menu-white.png");
-    $(".navbar").addClass( "navbar-white-color" );
-  }
+
+  $(window).scroll(function() {
+    var pos = $(this).scrollTop();
+    deviceWidth = $(window).width();
+    console.log(pos);
+    if (pos>348 && deviceWidth > 500){
+      $(".navbar").css("background-color", "white");
+      $("#nav-menu-image-btn").attr("src", "images/icons/cross-black.png");
+      $("#nav-menu-items").css("display", "flex");
+      $(".navbar").removeClass( "navbar-white-color" );
+    }
+    if (pos<348  && deviceWidth > 500){
+      $(".navbar").css("background-color", "transparent");
+      $("#nav-menu-image-btn").attr("src", "images/icons/menu-white.png");
+      $(".navbar").addClass( "navbar-white-color" );
+    }
+  });
+
 });
